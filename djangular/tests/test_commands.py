bdd74b68956b6794f78547d54d8b109bb3618e5c
Marc Hernández Cabot <mhernandez@apsl.net>
2018-10-04 10:21:45 +0200

    fixing some tests

--- djangular/tests/test_commands.py
+++ djangular/tests/test_commands.py
@@ -10,9 +10,10 @@ from djangular.management.commands.startangularapp import Command as StartAngula
 class StartAngularAppCommandTests(TestCase):
     def setUp(self):
         # Clean up app directory that is created
-        test_dir = os.path.abspath(os.path.dirname(upath(__file__)))
-        demo_app_path = os.path.join(test_dir, '../../demo')
-        self.addCleanup(shutil.rmtree, demo_app_path)
+        # test_dir = os.path.abspath(os.path.dirname(upath(__file__)))
+        # demo_app_path = os.path.join(test_dir, '../../demo')
+        # self.addCleanup(shutil.rmtree, demo_app_path)
+        pass
 
     def test_runs(self):
         StartAngularAppCommand().handle('demo', verbosity=1)
